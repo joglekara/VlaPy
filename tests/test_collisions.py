collision tests had a broadcasting bug. now fixed.
also added some complexity to the conservation of momentum testing

tests/test_collisions.py
@@ -21,6 +21,7 @@
 # SOFTWARE.
 
 from itertools import product
+from copy import deepcopy
 
 from tests import helpers
 from vlapy.core import step
@@ -30,12 +31,14 @@ import numpy as np
 ALL_SOLVERS = ["naive", "batched_tridiagonal"]
 ALL_OPERATORS = ["lb", "dg"]
 
+TOLERANCE = 4
+
 
 def __initialize_for_collisions__(vshift):
-    nx = 1
-    nv = 256
+    nx = 2
+    nv = 1024
 
-    nu = 1e-3
+    nu = 1e-2
     dt = 0.1
     v0 = 1.0
 
@@ -65,16 +68,15 @@ def __run_collision_operator_test_loop__(
 
     all_params = {
         "fokker-planck": {"type": collision_operator, "solver": solver},
-        "nu": 1e-3,
+        "nu": nu,
     }
 
     fp_step = step.get_collision_step(
         all_params=all_params, stuff_for_time_loop=stuff_for_time_loop
     )
 
-    f_out = f.copy()
+    f_out = deepcopy(f)
     for it in range(t_end):
-
         f_out = fp_step(f_out)
 
     return f, f_out, v, dv
@@ -96,7 +98,7 @@ def __test_maxwellian_solution__(collision_operator, solver):
         vshift=0.0, t_end=32, collision_operator=collision_operator, solver=solver
     )
 
-    np.testing.assert_almost_equal(f, f_out, decimal=6)
+    np.testing.assert_almost_equal(f, f_out, decimal=4)
 
 
 def test_energy_conservation():
@@ -114,9 +116,9 @@ def __test_energy_conservation__(collision_operator, solver):
         vshift=0.5, t_end=32, collision_operator=collision_operator, solver=solver
     )
 
-    temp_in = np.trapz(f[None,] * v ** 2.0, dx=dv, axis=1)
-    temp_out = np.trapz(f_out[None,] * v ** 2.0, dx=dv, axis=1)
-    np.testing.assert_almost_equal(temp_out, temp_in, decimal=6)
+    temp_in = np.trapz(f * v[None, :] ** 2.0, dx=dv, axis=1)
+    temp_out = np.trapz(f_out * v[None, :] ** 2.0, dx=dv, axis=1)
+    np.testing.assert_almost_equal(temp_out, temp_in, decimal=4)
 
 
 def test_density_conservation():
@@ -135,12 +137,12 @@ def __test_density_conservation__(collision_operator, solver):
         vshift=0.5, t_end=32, collision_operator=collision_operator, solver=solver
     )
 
-    temp_in = np.trapz(f[None,], dx=dv, axis=1)
-    temp_out = np.trapz(f_out[None,], dx=dv, axis=1)
-    np.testing.assert_almost_equal(temp_out, temp_in, decimal=6)
+    temp_in = np.trapz(f, dx=dv, axis=1)
+    temp_out = np.trapz(f_out, dx=dv, axis=1)
+    np.testing.assert_almost_equal(temp_out, temp_in, decimal=TOLERANCE)
 
 
-def test_lenard_bernstein_momentum_conservation_if_initialized_at_zero():
+def test_momentum_conservation_if_initialized_at_zero():
     for collision_operator, solver in product(ALL_OPERATORS, ALL_SOLVERS):
         __test_momentum_conservation_if_initialized_at_zero__(
             collision_operator=collision_operator, solver=solver
@@ -149,21 +151,21 @@ def test_lenard_bernstein_momentum_conservation_if_initialized_at_zero():
 
 def __test_momentum_conservation_if_initialized_at_zero__(collision_operator, solver):
     """
-    tests if the 0th moment of f is conserved
+    tests if the 1st moment of f is conserved if initialized at 0
 
     :return:
     """
     f, f_out, v, dv = __run_collision_operator_test_loop__(
-        vshift=0.0, t_end=32, collision_operator=collision_operator, solver=solver
+        vshift=0.0, t_end=512, collision_operator=collision_operator, solver=solver
     )
 
-    temp_in = np.trapz(f[None,] * v, dx=dv, axis=1)
-    temp_out = np.trapz(f_out[None,] * v, dx=dv, axis=1)
+    temp_in = np.trapz(f * v[None, :], dx=dv, axis=1)
+    temp_out = np.trapz(f_out * v[None, :], dx=dv, axis=1)
 
-    np.testing.assert_almost_equal(actual=temp_in, desired=temp_out, decimal=6)
+    np.testing.assert_almost_equal(actual=temp_in, desired=temp_out, decimal=TOLERANCE)
 
 
-def test_dougherty_momentum_conservation():
+def test_momentum_conservation():
     for collision_operator, solver in product(ALL_OPERATORS, ALL_SOLVERS):
         __test_momentum_conservation__(
             collision_operator=collision_operator, solver=solver
@@ -172,41 +174,27 @@ def test_dougherty_momentum_conservation():
 
 def __test_momentum_conservation__(collision_operator, solver):
     """
-    tests if the 0th moment of f is conserved
+    Tests if the 1st moment of f is conserved or not
+
+    This moment is not conserved and is brought to 0 by the following operators:
+    1 - "lb"
+
+    This moment is conserved by the following operators:
+    1 - "dg"
+
 
     :return:
     """
     f, f_out, v, dv = __run_collision_operator_test_loop__(
-        vshift=0.1, collision_operator=collision_operator, solver=solver
+        vshift=0.1, collision_operator=collision_operator, solver=solver, t_end=512,
     )
 
-    temp_in = np.trapz(f[None,] * v, dx=dv, axis=1)
-    temp_out = np.trapz(f_out[None,] * v, dx=dv, axis=1)
+    temp_in = np.trapz(f * v[None, :], dx=dv, axis=1)
+    temp_out = np.trapz(f_out * v[None, :], dx=dv, axis=1)
 
-    np.testing.assert_almost_equal(actual=temp_in, desired=temp_out, decimal=6)
-
-
-# def test_lenard_bernstein_velocity_zero_np():
-#     __test_lenard_bernstein_velocity_zero(solver="numpy")
-#     pass
-#
-#
-# def test_lenard_bernstein_velocity_zero_bt():
-#     __test_lenard_bernstein_velocity_zero(solver="batched_tridiagonal")
-#     pass
-#
-#
-# def __test_lenard_bernstein_velocity_zero(solver, collision_operator):
-#     """
-#     tests if the 1st moment of f is (approximately) 0
-#
-#     :return:
-#     """
-#
-#     f, f_out, v, dv = helpers.__run_collision_operator_test_loop(
-#         vshift=0.1, solver=solver, t_end=1000, collision_operator=collision_operator,
-#     )
-#
-#     temp_out = np.trapz(f_out[None,] * v, dx=dv, axis=1)
-#
-#     np.testing.assert_almost_equal(actual=0.0, desired=temp_out, decimal=4)
+    if collision_operator == "lb":
+        np.testing.assert_array_less(x=temp_out, y=temp_in)
+    elif collision_operator == "dg":
+        np.testing.assert_almost_equal(
+            actual=temp_in, desired=temp_out, decimal=TOLERANCE
+        )